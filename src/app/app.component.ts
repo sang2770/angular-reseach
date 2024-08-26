79c457cb9f4bd9a3bdc6a56465e8eada90f41b7f
xuansang <nguyenvansang30102001@gmail.com>
2024-08-26 22:20:49 +0700

    add ant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   readonly ROUTER_UTILS = ROUTER_UTILS;
+
+  public isCollapsed = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routes } from './app-routing.module';
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NzLayoutModule,
+    NzIconModule,
+    NzMenuModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
